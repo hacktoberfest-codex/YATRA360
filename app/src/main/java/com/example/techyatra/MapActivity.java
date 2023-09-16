3c710093a73918834c800af2be096d78fc71f68a
Rashmi <ranjanrashmi994@gmail.com>
2023-09-17 02:16:23 +0530

    6th commit

--- app/src/main/java/com/example/techyatra/MapActivity.java
+++ app/src/main/java/com/example/techyatra/MapActivity.java
@@ -2,5 +2,8 @@ package com.example.techyatra;
 
 import android.app.Activity;
 
+import com.google.android.gms.maps.GoogleMap;
+
 public class MapActivity extends Activity {
+    private GoogleMap mMap;
 }
