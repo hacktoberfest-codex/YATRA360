289fbeff78150a7c8ff261d6cc707fe174352e0e
Rashmi <ranjanrashmi994@gmail.com>
2023-09-17 02:19:56 +0530

    7th commit

--- app/src/main/java/com/example/techyatra/MapActivity.java
+++ app/src/main/java/com/example/techyatra/MapActivity.java
@@ -3,7 +3,9 @@ package com.example.techyatra;
 import android.app.Activity;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
 
 public class MapActivity extends Activity {
     private GoogleMap mMap;
+    private Marker haryanaMarker;
 }
