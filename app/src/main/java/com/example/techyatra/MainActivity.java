d72d58a34e425c078c138016525efac06408d0ef
yesiamrajeev <125568812+yesiamrajeev@users.noreply.github.com>
2023-09-17 01:48:40 +0530

    5th commit

--- app/src/main/java/com/example/techyatra/MainActivity.java
+++ app/src/main/java/com/example/techyatra/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.techyatra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GoogleApiAvailabilityLight;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (isServiceOK()) {
+            init();
+        }
     }
+    private void init() {
+        Button btnmap = findViewById(R.id.btnmap);
+        btnmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+    }
+
+    public boolean isServiceOK() {
+        Log.d(TAG, "isServiceOK: checking Google services");
+        int available = GoogleApiAvailabilityLight.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        if (available == ConnectionResult.SUCCESS) {
+            Log.d(TAG, "isServiceOK: WORKING");
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            Log.d(TAG, "isServiceOK: error but fixed");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "You cannot do anything", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 }
---
+++ app/src/main/java/com/example/techyatra/MapActivity.java
@@ -0,0 +1,6 @@
+package com.example.techyatra;
+
+import android.app.Activity;
+
+public class MapActivity extends Activity {
+}
