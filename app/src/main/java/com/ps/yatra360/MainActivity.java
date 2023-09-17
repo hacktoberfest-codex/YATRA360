4e41dc9a326cc6559bad64a886f13f209a2fb169
yesiamrajeev <125568812+yesiamrajeev@users.noreply.github.com>
2023-09-17 17:06:31 +0530

    FINAL COMMIT Part-2

--- app/src/main/java/com/ps/yatra360/MainActivity.java
+++ app/src/main/java/com/ps/yatra360/MainActivity.java
@@ -9,6 +9,7 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +23,35 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private final int CAMERA_REQ_CODE = 100;
     FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btnmap = findViewById(R.id.signupbutton2);
+        Button signupbutton1 = findViewById(R.id.signupbutton1);
+
+        signupbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent icamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(icamera, CAMERA_REQ_CODE);
+            }
+        });
+        // Create a GradientDrawable with the desired colors
+        Button btn2map = findViewById(R.id.signupbutton2);  // edit it laater map button
+        //done
+        btn2map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
         btnmap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
